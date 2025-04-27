68fe703a3ce77974001702ae032795280557567c
andromaan <146390373+andromaan@users.noreply.github.com>
2025-04-27 17:50:57 +0300

    added new logs and fix reminders

--- TermPaperApi/src/Application/Services/ReminderService/ReminderService.cs
+++ TermPaperApi/src/Application/Services/ReminderService/ReminderService.cs
@@ -3,25 +3,18 @@ using Hangfire;
 
 namespace Application.Services.ReminderService;
 
-public class ReminderService : IReminderService
+public class ReminderService(IEmailService emailService) : IReminderService
 {
-    private readonly IEmailService _emailService;
-
-    public ReminderService(IEmailService emailService)
-    {
-        _emailService = emailService;
-    }
-
     public void ScheduleReminder(string userEmail, string title, DateTime reminderTime)
     {
-        BackgroundJob.Schedule(() => SendReminder(userEmail, title), reminderTime - DateTime.Now);
+        BackgroundJob.Schedule(() => SendReminder(userEmail, title), reminderTime - DateTime.UtcNow);
     }
 
     public void SendReminder(string userEmail, string title)
     {
         var subject = $"Нагадування: {title}";
         var body = $"Шановний користувач, нагадуємо, що скоро настане подія: {title}. Будь ласка, підготуйтесь.";
-        
-        BackgroundJob.Enqueue(() => _emailService.SendEmail(userEmail, subject, body));
+
+        BackgroundJob.Enqueue(() => emailService.SendEmail(userEmail, subject, body));
     }
 }
